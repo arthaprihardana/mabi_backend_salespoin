f56a8b87e2b3e00dac0503ca62eb67b70ee6466f
Artha Prihardana <artha.prihardana@redbuzz.co.id>
2018-04-18 13:34:29 +0700

    update sampai model

---
+++ src/models/lokasi.js
@@ -0,0 +1,6 @@
+/**
+ * @author: Artha Prihardana 
+ * @Date: 2018-04-18 13:33:42 
+ * @Last Modified by:   Artha Prihardana 
+ * @Last Modified time: 2018-04-18 13:33:42 
+ */
---
+++ src/models/user.js
@@ -0,0 +1,97 @@
+/**
+ * @author: Artha Prihardana 
+ * @Date: 2018-04-18 11:18:15 
+ * @Last Modified by: Artha Prihardana
+ * @Last Modified time: 2018-04-18 11:38:43
+ */
+import validasi from '../lib/validasi';
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+    nama: {
+        type: String,
+        required: true
+    },
+    tempatLahir: {
+        type: String
+    },
+    tanggalLahir: {
+        type: Date
+    },
+    alamat: {
+        type: String
+    },
+    email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        validate: [validasi.email, "Silahkan masukan alamat email dengan format yang valid"]
+    },
+    noHandphone: {
+        type: String,
+        required: true,
+        validate: [validasi.phone, "Silahkan masukan no handphone dengan format yang valid"]
+    },
+    role: {
+        type: String,
+        enum: ["admin", "agen"],
+        default: "admin",
+        lowercase: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    show: {
+        type: Boolean,
+        default: true
+    },
+    token: {
+        type: String
+    },
+    senderId: {
+        type: String
+    }
+}, { 
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
+
+UserSchema.path('nama').validate((val, cb) => {
+    UserModel.find({ nama: val }, (err, docs) => {
+        if(docs.length > 0) {
+            cb(false, 'Nama user telah terdaftar');
+        } else {
+            cb(true);
+        }
+    });
+});
+
+UserSchema.path('email').validate((val, cb) => {
+    UserModel.find({ email: val }, (err, docs) => {
+        if(docs.length > 0) {
+            cb(false, 'Email user telah terdaftar');
+        } else {
+            cb(true);
+        }
+    });
+});
+
+UserSchema.path('noHandphone').validate((val, cb) => {
+    UserModel.find({ noHandphone: val }, (err, docs) => {
+        if(docs.length > 0) {
+            cb(false, 'No Handphone user telah terdaftar');
+        } else {
+            cb(true);
+        }
+    });
+});
+
+const UserModel = mongoose.model('user', UserSchema);
+export default UserModel;
